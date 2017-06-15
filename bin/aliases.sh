cfad142ce4ede39f540dbf551a80a4758240c4a4
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2017-06-15 15:12:56 -0700

    Scripts moved to bin folder

---
+++ bin/aliases.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+alias gocedar='cd $CEDAR_HOME'
+alias godocker='cd $CEDAR_DOCKER_HOME'
+
+alias gobuild='cd $CEDAR_HOME/cedar-docker-build/'
+alias godeploy='cd $CEDAR_HOME/cedar-docker-deploy/'
+alias gofrontend='cd $CEDAR_HOME/cedar-docker-deploy/cedar-frontend'
+alias goinfrastructure='cd $CEDAR_HOME/cedar-docker-deploy/cedar-infrastructure'
+alias gomicroservices='cd $CEDAR_HOME/cedar-docker-deploy/cedar-microservices'
+alias gomonitoring='cd $CEDAR_HOME/cedar-docker-deploy/cedar-monitoring'
+
+alias startfrontend='gofrontend && docker-compose up'
+alias startmonitoring='gomonitoring && docker-compose up'
+alias startmicroservices='gomicroservices && docker-compose up'
+alias startinfrastructure='goinfrastructure && docker-compose up'
+
+alias stopfrontend='gofrontend && docker-compose down'
+alias stopmonitoring='gomonitoring && docker-compose down'
+alias stopmicroservices='gomicroservices && docker-compose down'
+alias stopinfrastructure='goinfrastructure && docker-compose down'
+
+alias cedarss='$CEDAR_HOME/cedar-docker-deploy/bin/cedarstatus.sh'
+
--- copy-certificates.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Copying CEDAR self signed certificates"
-cp ${CEDAR_DOCKER_HOME}/cedar-docker-deploy/cedar-assets/ca/ca-cedar.crt ${CEDAR_CA_HOME}/
-cp ${CEDAR_DOCKER_HOME}/cedar-docker-deploy/cedar-assets/cert/cedar.crt ${CEDAR_CERT_HOME}/
-cp ${CEDAR_DOCKER_HOME}/cedar-docker-deploy/cedar-assets/cert/cedar.key ${CEDAR_CERT_HOME}/
