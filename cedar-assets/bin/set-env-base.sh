b21003d93a236f91e386e4cb04add3346555b3a5
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2017-08-08 16:09:24 -0700

    Immport submission password added

--- cedar-assets/bin/set-env-base.sh
+++ cedar-assets/bin/set-env-base.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-export CEDAR_VERSION=1.3.3-SNAPSHOT
+#CEDAR version
+export CEDAR_VERSION="1.3.3-SNAPSHOT"
+#CEDAR version modifier
 export CEDAR_VERSION_MODIFIER=""
  
 #CEDAR BioPortal API Key - place your own key here
@@ -9,5 +11,7 @@ export CEDAR_BIOPORTAL_API_KEY="changeme-bbbb-cccc-dddd-eeeeeeeeeeee"
 export CEDAR_ANALYTICS_KEY="false"
 #CEDAR NCBI SRA FTP Password - place your own password here
 export CEDAR_NCBI_SRA_FTP_PASSWORD="changeme"
+#CEDAR ImmPort Submission Password - place your own password here
+export CEDAR_IMMPORT_SUBMISSION_PASSWORD="changeme"
 #Random string for API key generation - replace with a random string of your choice
 export CEDAR_SALT_API_KEY="changeme"
