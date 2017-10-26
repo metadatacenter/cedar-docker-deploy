28d5d37141a7b5a90772617935c88d2565d14f46
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2017-10-26 13:55:34 -0700

    Added check before deleting cedarnet

--- bin/create-network.sh
+++ bin/create-network.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-echo "Removing previous Docker network"
-docker network remove cedarnet
+echo "Checking previous Docker network ..."
+if docker network ls | grep "cedarnet" > /dev/null 2>&1
+then
+    echo "Removing previous Docker network ..."
+    docker network remove cedarnet
+else
+    echo "Previous network not present, nothing to do."
+    echo
+fi
 
-echo "Creating Docker network: cedarnet"
+echo "Creating Docker network: cedarnet ..."
 docker network create --subnet=${CEDAR_NET_SUBNET}/24 --gateway ${CEDAR_NET_GATEWAY} cedarnet
