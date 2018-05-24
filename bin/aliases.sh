730acf80503f6c7c7d20d85ed4a0aeb557134d2c
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2018-05-24 16:34:04 -0700

    Neo4j with volumes

--- bin/aliases.sh
+++ bin/aliases.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-alias godocker='cd $CEDAR_DOCKER_HOME'
+alias godocker='cd $CEDAR_DOCKER_SRC_HOME'
 
-alias gobuild='cd $CEDAR_DOCKER_HOME/cedar-docker-build/'
-alias godeploy='cd $CEDAR_DOCKER_HOME/cedar-docker-deploy/'
-alias gofrontend='cd $CEDAR_DOCKER_HOME/cedar-docker-deploy/cedar-frontend'
-alias goinfrastructure='cd $CEDAR_DOCKER_HOME/cedar-docker-deploy/cedar-infrastructure'
-alias gomicroservices='cd $CEDAR_DOCKER_HOME/cedar-docker-deploy/cedar-microservices'
-alias gomonitoring='cd $CEDAR_DOCKER_HOME/cedar-docker-deploy/cedar-monitoring'
+alias gobuild='cd $CEDAR_DOCKER_SRC_HOME/cedar-docker-build/'
+alias godeploy='cd $CEDAR_DOCKER_SRC_HOME/cedar-docker-deploy/'
+alias gofrontend='cd $CEDAR_DOCKER_SRC_HOME/cedar-docker-deploy/cedar-frontend'
+alias goinfrastructure='cd $CEDAR_DOCKER_SRC_HOME/cedar-docker-deploy/cedar-infrastructure'
+alias gomicroservices='cd $CEDAR_DOCKER_SRC_HOME/cedar-docker-deploy/cedar-microservices'
+alias gomonitoring='cd $CEDAR_DOCKER_SRC_HOME/cedar-docker-deploy/cedar-monitoring'
 
 alias startfrontend='gofrontend && docker-compose up'
 alias startmonitoring='gomonitoring && docker-compose up'
@@ -19,5 +19,5 @@ alias stopmonitoring='gomonitoring && docker-compose down'
 alias stopmicroservices='gomicroservices && docker-compose down'
 alias stopinfrastructure='goinfrastructure && docker-compose down'
 
-alias cedarss='$CEDAR_DOCKER_HOME/cedar-docker-deploy/bin/cedarstatus.sh'
+alias cedarss='$CEDAR_DOCKER_SRC_HOME/cedar-docker-deploy/bin/cedarstatus.sh'
 
--- bin/copy-certificates.sh
+++ bin/copy-certificates.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Copying CEDAR self signed certificates"
-cp ${CEDAR_DOCKER_HOME}/cedar-docker-deploy/cedar-assets/ca/ca-cedar.crt ${CEDAR_DOCKER_CA_HOME}/
-cp ${CEDAR_DOCKER_HOME}/cedar-docker-deploy/cedar-assets/cert/cedar.crt ${CEDAR_DOCKER_CERT_HOME}/
-cp ${CEDAR_DOCKER_HOME}/cedar-docker-deploy/cedar-assets/cert/cedar.key ${CEDAR_DOCKER_CERT_HOME}/
+cp ${CEDAR_DOCKER_SRC_HOME}/cedar-docker-deploy/cedar-assets/ca/ca-cedar.crt ${CEDAR_DOCKER_CA_HOME}/
+cp ${CEDAR_DOCKER_SRC_HOME}/cedar-docker-deploy/cedar-assets/cert/cedar.crt ${CEDAR_DOCKER_CERT_HOME}/
+cp ${CEDAR_DOCKER_SRC_HOME}/cedar-docker-deploy/cedar-assets/cert/cedar.key ${CEDAR_DOCKER_CERT_HOME}/
