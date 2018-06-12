2c1d09528692e0cf4925133a122a29a21b006fd7
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2018-06-11 17:42:28 -0700

    Separate certificates for each subdomain for development

--- bin/docker-certificate.sh
+++ bin/docker-certificate.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 docker run -v cedar_cert:/data --name cedar-cert-helper busybox true
-docker cp ${CEDAR_DOCKER_CERT_HOME}/cedar.crt cedar-cert-helper:/data
-docker cp ${CEDAR_DOCKER_CERT_HOME}/cedar.key cedar-cert-helper:/data
+for filename in ${CEDAR_DOCKER_CERT_HOME}/*; do
+  echo Copying $filename to docker volume cedar_cert
+  docker cp $filename cedar-cert-helper:/data
+done
 docker rm cedar-cert-helper
 
 docker run -v cedar_ca:/data --name cedar-ca-helper busybox true
+echo Copying ca-cedar.crt to docker volume cedar_ca
 docker cp ${CEDAR_DOCKER_CA_HOME}/ca-cedar.crt cedar-ca-helper:/data
 docker rm cedar-ca-helper
