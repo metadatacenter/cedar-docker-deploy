6aa301eb05da398283fe670c9b1d171f62fecd2f
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2018-06-13 15:45:05 -0700

    Shell script refactored

--- bin/original/set-env-external.sh
+++ bin/original/set-env-external.sh
@@ -2,9 +2,6 @@
 
 # This file contains connection information to external resources
 
-#CEDAR version modifier
-export CEDAR_VERSION_MODIFIER=""
- 
 #CEDAR BioPortal API Key - place your own key here
 export CEDAR_BIOPORTAL_API_KEY="changeme-bbbb-cccc-dddd-eeeeeeeeeeee"
 
@@ -20,6 +17,3 @@ export CEDAR_NCBI_SRA_FTP_DIRECTORY="directory/subdirectory"
 #CEDAR ImmPort Submission User and Password - place your own data here
 export CEDAR_IMMPORT_SUBMISSION_USER="changeme"
 export CEDAR_IMMPORT_SUBMISSION_PASSWORD="changeme"
-
-#Random string for API key generation - replace with a random string of your choice
-export CEDAR_SALT_API_KEY="changeme"
