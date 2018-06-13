2e7842119ad7003e28eaa3c352afcd1e6bd69d80
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2018-06-13 14:24:06 -0700

    Changed cert copy mechanism

--- bin/copy-certificate.sh
+++ bin/copy-certificate.sh
@@ -2,10 +2,10 @@
 
 docker run -v cedar_cert:/data --name cedar-cert-helper busybox true
 echo Copying live to docker volume cedar_cert
-docker cp ${CEDAR_DOCKER_CERT_HOME}/live cedar-cert-helper:/data
+docker cp ${CEDAR_DOCKER_SRC_HOME}/cedar-docker-deploy/cedar-assets/cert/live cedar-cert-helper:/data
 docker rm cedar-cert-helper
 
 docker run -v cedar_ca:/data --name cedar-ca-helper busybox true
 echo Copying ca-cedar.crt to docker volume cedar_ca
-docker cp ${CEDAR_DOCKER_CA_HOME}/ca-cedar.crt cedar-ca-helper:/data
+docker cp ${CEDAR_DOCKER_SRC_HOME}/cedar-docker-deploy/cedar-assets/ca-cedar.crt cedar-ca-helper:/data
 docker rm cedar-ca-helper
