22fe22fb8720ba5557f9f33af5e319e5dfcd2469
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2018-06-19 10:41:31 -0700

    Fix path

--- bin/util/services/stopinfra.sh
+++ bin/util/services/stopinfra.sh
@@ -5,12 +5,12 @@ echo Stopping CEDAR infrastructure services
 echo --------------------------------------------------------------------------------
 echo
 
-$CEDAR_HOME/cedar-util/bin/killkeycloak.sh
-$CEDAR_HOME/cedar-util/bin/stopmongo.sh
-$CEDAR_HOME/cedar-util/bin/stopelastic.sh
-$CEDAR_HOME/cedar-util/bin/stopkibana.sh
-$CEDAR_HOME/cedar-util/bin/stopneo.sh
-$CEDAR_HOME/cedar-util/bin/killrediscommander.sh
-$CEDAR_HOME/cedar-util/bin/stopredis.sh
-$CEDAR_HOME/cedar-util/bin/stopmysql.sh
-$CEDAR_HOME/cedar-util/bin/stopnginx.sh
+$CEDAR_UTIL_BIN/services/bin/killkeycloak.sh
+$CEDAR_UTIL_BIN/services/bin/stopmongo.sh
+$CEDAR_UTIL_BIN/services/bin/stopelastic.sh
+$CEDAR_UTIL_BIN/services/bin/stopkibana.sh
+$CEDAR_UTIL_BIN/services/bin/stopneo.sh
+$CEDAR_UTIL_BIN/services/bin/killrediscommander.sh
+$CEDAR_UTIL_BIN/services/bin/stopredis.sh
+$CEDAR_UTIL_BIN/services/bin/stopmysql.sh
+$CEDAR_UTIL_BIN/services/bin/stopnginx.sh
