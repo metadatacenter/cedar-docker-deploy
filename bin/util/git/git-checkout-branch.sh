791b73d247e4d62b952119e17a416aa1b1cbd648
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2018-06-20 13:11:19 -0700

    Make it fool proof ($CEDAR_HOME/) and replace tabs with spaces

--- bin/util/git/git-checkout-branch.sh
+++ bin/util/git/git-checkout-branch.sh
@@ -5,17 +5,17 @@ source $CEDAR_UTIL_BIN/include-repo-list.sh
 format="\n\nSwitching Git repo ${GREEN}%-20s${NORMAL} : (%-60s)\n"
 
 function switchToRepo {
-	printf "$format" $1 $CEDAR_HOME$1
-	git -C "$CEDAR_HOME$1" checkout $2
-	if [ $? == 0 ]; then
-		echo "${GREEN}Up-to-date with remote :)${NORMAL}"
-	else
-		echo "${RED}Something went wrong here!${NORMAL}"
-	fi
-	git -C "$CEDAR_HOME$1" pull
+  printf "$format" $1 $CEDAR_HOME/$1
+  git -C "$CEDAR_HOME/$1" checkout $2
+  if [ $? == 0 ]; then
+    echo "${GREEN}Up-to-date with remote :)${NORMAL}"
+  else
+    echo "${RED}Something went wrong here!${NORMAL}"
+  fi
+  git -C "$CEDAR_HOME/$1" pull
 }
 
 for i in "${CEDAR_REPOS[@]}"
 do
-   switchToRepo $i $@
+  switchToRepo $i $@
 done
