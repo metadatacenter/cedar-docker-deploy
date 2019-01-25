f3b1d48545ed7e474b451cb1324a7de979827c90
Attila Levente EGYEDI <attila.egyedi@gmail.com>
2019-01-25 10:06:50 -0800

    Add delays between server starts

--- bin/util/services/startall.sh
+++ bin/util/services/startall.sh
@@ -8,15 +8,27 @@ shopt -s expand_aliases
 source $CEDAR_UTIL_BIN/set-dev-aliases.sh
 
 startworkspace
+sleep 1
 startgroup
+sleep 1
 startmessaging
+sleep 1
 startrepo
+sleep 1
 startresource
+sleep 1
 startschema
+sleep 1
 starttemplate
+sleep 1
 startterminology
+sleep 1
 startuser
+sleep 1
 startvaluerecommender
+sleep 1
 startsubmission
+sleep 1
 startworker
+sleep 1
 startopen
