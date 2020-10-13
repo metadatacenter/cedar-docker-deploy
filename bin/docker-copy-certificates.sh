e5f6d5f169e43e03fa9bac5372f6e006029600b6
Attila L. Egyedi <attila.egyedi@gmail.com>
2020-10-13 14:09:00 -0700

    Removed old docs

--- bin/docker-copy-certificates.sh
+++ bin/docker-copy-certificates.sh
@@ -7,5 +7,5 @@ docker rm cedar-cert-helper
 
 echo "Copying CA certificate into the cedar_ca volume..."
 docker run -v cedar_ca:/data --name cedar-ca-helper busybox:1.28.4 true
-docker cp ${CEDAR_DOCKER_HOME}/cedar-docker-deploy/cedar-assets/ca/ca-cedar.crt cedar-ca-helper:/data
+docker cp ${CEDAR_DOCKER_HOME}/cedar-docker-deploy/cedar-assets/ca/ca.crt cedar-ca-helper:/data
 docker rm cedar-ca-helper
