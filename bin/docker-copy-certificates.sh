99599058de17c8c83752d7e44db57a8e1c8d207e
Attila L. Egyedi <egyedia@stanford.edu>
2023-05-22 16:46:53 -0700

    Remove CEDAR_DOCKER_HOME

--- bin/docker-copy-certificates.sh
+++ bin/docker-copy-certificates.sh
@@ -2,10 +2,10 @@
 
 echo "Copying self-signed certificates into the cedar_cert volume..."
 docker run -v cedar_cert:/data --name cedar-cert-helper busybox:1.36.0 true
-docker cp ${CEDAR_DOCKER_HOME}/cedar-docker-deploy/cedar-assets/cert/live cedar-cert-helper:/data
+docker cp ${CEDAR_HOME}/cedar-docker-deploy/cedar-assets/cert/live cedar-cert-helper:/data
 docker rm cedar-cert-helper
 
 echo "Copying CA certificate into the cedar_ca volume..."
 docker run -v cedar_ca:/data --name cedar-ca-helper busybox:1.36.0 true
-docker cp ${CEDAR_DOCKER_HOME}/cedar-docker-deploy/cedar-assets/ca/ca.crt cedar-ca-helper:/data
+docker cp ${CEDAR_HOME}/cedar-docker-deploy/cedar-assets/ca/ca.crt cedar-ca-helper:/data
 docker rm cedar-ca-helper
